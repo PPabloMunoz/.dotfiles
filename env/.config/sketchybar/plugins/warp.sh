b3730fbb40dc60cb6bb76929ea36380668ae79ce
Pablo Muñoz <ppablomunozp@gmail.com>
2025-05-14 21:07:35 +0200

    updates

--- env/.config/sketchybar/plugins/warp.sh
+++ env/.config/sketchybar/plugins/warp.sh
@@ -11,11 +11,11 @@ STATUS=$(/usr/local/bin/warp-cli status 2>/dev/null)
 
 # Use grep to check status
 if echo "$STATUS" | grep -q "Disconnected"; then
-    sketchybar --set "$NAME" background.color=0xfff38ba8 icon=""
+    sketchybar --set "$NAME" icon.color=0xfff38ba8 icon=""
 elif echo "$STATUS" | grep -q "Connected"; then
-    sketchybar --set "$NAME" background.color=0xfffab387 icon="󱇱"
+    sketchybar --set "$NAME" icon.color=0xfffab387 icon="󱇱"
 elif echo "$STATUS" | grep -q "Connecting"; then
-    sketchybar --set "$NAME" background.color=0xff89b4fa icon="󰘿"
+    sketchybar --set "$NAME" icon.color=0xff89b4fa icon="󰘿"
 else
-    sketchybar --set "$NAME" background.color=0xff89b4fa icon=""
+    sketchybar --set "$NAME" icon.color=0xfff38ba8 icon=""
 fi
