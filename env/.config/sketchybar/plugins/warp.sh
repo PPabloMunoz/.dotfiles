29155a1a1c9822b82a850bc0903b646eb58ea516
Pablo Muñoz <ppablomunozp@gmail.com>
2025-05-16 11:11:06 +0200

    update vpn icon to green when connected

--- env/.config/sketchybar/plugins/warp.sh
+++ env/.config/sketchybar/plugins/warp.sh
@@ -13,7 +13,7 @@ STATUS=$(/usr/local/bin/warp-cli status 2>/dev/null)
 if echo "$STATUS" | grep -q "Disconnected"; then
     sketchybar --set "$NAME" icon.color=0xfff38ba8 icon=""
 elif echo "$STATUS" | grep -q "Connected"; then
-    sketchybar --set "$NAME" icon.color=0xfffab387 icon="󱇱"
+    sketchybar --set "$NAME" icon.color=0xffa6e3a1 icon="󱇱"
 elif echo "$STATUS" | grep -q "Connecting"; then
     sketchybar --set "$NAME" icon.color=0xff89b4fa icon="󰘿"
 else
