c1f7d89fd67facef237ec32a871c80f953a31e3c
Pablo Muñoz <ppablomunozp@gmail.com>
2025-09-10 09:23:17 +0200

    fix to show hidden files on explorer and picker while hidding dependencies folders

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,7 +2,18 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      hidden = true,
+      ignored = true,
+      exclude = {
+        "node_modules", -- hide node modules
+        "vendor", -- hide Go vendor folder
+        ".git", -- hide git folder
+      },
       sources = {
+        files = {
+          hidden = true, -- Ensure file source shows hidden files
+          ignored = true, -- Also respect gitignore for that source
+        },
         explorer = {
           auto_close = true,
           win = {
