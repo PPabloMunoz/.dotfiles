b17dddda2ec025b5eb55329a4e89a792128fee1b
Pablo Muñoz <ppablomunozp@gmail.com>
2025-09-21 12:15:54 +0200

    update arch install script - add carapace and starship

--- arch_install.sh
+++ arch_install.sh
@@ -28,6 +28,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+yay -S starship-git
 
 yay -S zoxide eza bat ripgrep fd fzf jq fastfetch tlrc stow feh brightnessctl xorg-xinit
 yay -S neovim tmux fnm go
