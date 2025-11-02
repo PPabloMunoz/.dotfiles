0a1f6bb0985e62168665ee75c7a227075a551be2
Pablo Muñoz <ppablomunozp@gmail.com>
2025-11-02 21:42:26 +0100

    Add lazygit plugin

--- nvim/.config/nvim/lua/custom/init.lua
+++
@@ -1,7 +0,0 @@
--- Autoformat C/C++ on save using clangd
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = { '*.c', '*.cpp', '*.h' },
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end,
-})
