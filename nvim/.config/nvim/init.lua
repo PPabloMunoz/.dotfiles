23da42a08bc8b250870bac9395be199e8fe253a4
Pablo Muñoz <ppablomunozp@gmail.com>
2025-11-02 21:50:47 +0100

    add default shiftwidth to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,6 +45,11 @@ vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+
 vim.o.splitright = true
 vim.o.splitbelow = true
 
